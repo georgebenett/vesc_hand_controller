61127b3503400ab5090a439d91448e45c5590850
george benett <georgebenett@gmail.com>
2024-05-29 13:56:02 +0200

    deleted uneccessary files from receiver and implemented safety throttle response in case of disconnection

--- receiver/src/main.cpp
+++ receiver/src/main.cpp
@@ -1,41 +1,8 @@
 #include <Arduino.h>
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <SPI.h>
 #include <esp_now.h>
 #include <WiFi.h>
 
-
-#include "throttle.h"
-#include "battery.h"
-
-
-#define TFT_CS          D4
-#define TFT_RST         D6
-#define TFT_DC          D5
-
-#define TIMER_INTERVAL  10000
-
-/*
-#define TFT_SCL      D8
-#define TFT_SDA      D10 just info */
-
-Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
-
-/*TO-DO: define int sizes*/
-int throttle_readings[NUM_READINGS];  // Array to store throttle readings
-int read_index = 0;                   // Index of the current reading
-int total = 0;                        // Running total of the readings
-int average_throttle = 0;             // Average throttle value
-int raw_throttle_value = 0;           // Raw throttle value
-int old_throttle = 0;                 // Old throttle value
-int current_throttle_value = 0;       // Current throttle value
-
-int raw_battery_voltage =  0;
-int battery_voltage = 0;
-int old_battery_voltage = 0;                  // Old battery voltage
-int battery_charge = 0;
-int old_battery_charge = 0;
 
 //esp-now broadcast address
 uint8_t broadcastAddress[] = {0xEC, 0xDA, 0x3B, 0xBF, 0x5D, 0xD0};
@@ -46,6 +13,7 @@ int incomingThrottle;
 // Variable to store if sending data was successful
 String success;
 
+
 typedef struct struct_message {
     int throttle;
 } struct_message;
@@ -55,41 +23,27 @@ struct_message myData;
 
 esp_now_peer_info_t peerInfo;
 
+// Callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  //Serial.print("Last Packet Send Status: ");
+  //Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  if (status ==0){
+    myData.throttle = incomingThrottle;
+  }
+  else{
+    myData.throttle = 0;
+  }
+}
 
 // Callback when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&myData, incomingData, sizeof(myData));
   incomingThrottle = myData.throttle;
-
 }
 
-// create a timer with default settings
-auto timer = timer_create_default();
-
-bool timerCallBatteryCharge(void *) {
-  printBatteryCharge();
-  return true; // repeat? true
-}
 
 void setup(void) {
   Serial.begin(115200);
-  //set lcd properties
-  tft.init(240, 280);           // Init ST7789 280x240
-  tft.setRotation(0);
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.setTextSize(2);
-
-  //set the timers
-  timer.every(TIMER_INTERVAL, timerCallBatteryCharge);
-
-  // Initialize all readings to 0
-  for (uint8_t i = 0; i < NUM_READINGS; i++) {
-    throttle_readings[i] = 0;
-  }
-  // inicialize functions
-  printBatteryVoltage();
-  printBatteryCharge();
 
   WiFi.mode(WIFI_MODE_STA);
 
@@ -99,7 +53,9 @@ void setup(void) {
     return;
   }
 
-
+    // Once ESPNow is Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
 
   // Register peer
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
@@ -118,11 +74,10 @@ void setup(void) {
 
 
 void loop() {
-  printAverageThrottle();
-  printBatteryVoltage();
+    // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
 
-  timer.tick(); /*This one calls the print battery charge function*/
+  Serial.println(myData.throttle);
 
-  Serial.println(incomingThrottle);
 }
 
